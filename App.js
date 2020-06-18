5a84375d4dafc50f264153f27d7c6747aa974685
Nick Tillinghast <nickt2@cox.net>
2020-06-17 22:20:34 -0700

    reducer in place of redux

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './scr/screens/IndexScreen';
-import { BlogProvider } from './scr/context/BlogContext';
+import { Provider } from './scr/context/BlogContext';
 
 
 const navigator = createStackNavigator({
@@ -17,8 +17,8 @@ const navigator = createStackNavigator({
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <BlogProvider>
+  return <Provider>
     <App />
-  </BlogProvider>
+  </Provider>
 };
 
